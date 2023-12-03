504303910da2d3d31fe9984d7fda1594ec074622
roudane2021 <ra.roudane@gmail.com>
2023-12-03 14:33:45 +0100

    correction

--- layer-repository-paiements/src/main/java/ma/roudane/repositories/IPaiementRepository.java
+++
@@ -1,9 +0,0 @@
-package ma.roudane.repositories;
-
-
-import ma.roudane.entities.PaiementEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IPaiementRepository extends JpaRepository<PaiementEntity, Integer> {
-    PaiementEntity findByidCommande(int idCommande);
-}
