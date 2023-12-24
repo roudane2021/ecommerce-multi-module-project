09db9bb9522270cf78bbf100b24a83ae66088150
roudane2021 <ra.roudane@gmail.com>
2023-12-24 22:01:16 +0100

    scroll infinti

---
+++ ecommerce-ihm/src/app/produit-achat/components/list-produit/filter-produit/filter-produit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-produit',
+  templateUrl: './filter-produit.component.html',
+  styleUrls: ['./filter-produit.component.scss']
+})
+export class FilterProduitComponent {
+
+}
--- ecommerce-ihm/src/app/produit-achat/produit-achat-routing.module.ts
+++ ecommerce-ihm/src/app/produit-achat/produit-achat-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListProduitComponent } from './components/list-produit/list-produit.component';
 import { MainComponent } from './components/main/main.component';
+import { ProduitItemComponent } from './components/produit-item/produit-item.component';
 
 const routes: Routes = [
   {path: 'main', component: MainComponent, 
   children: [
-    {path: 'listProduit', component: ListProduitComponent},
-    {path:'', pathMatch: 'full', redirectTo: 'listProduit'}
+    {path: 'produits', component: ListProduitComponent},
+    {path: 'produits/:id', component: ProduitItemComponent},
+    {path:'', pathMatch: 'full', redirectTo: 'produits'}
   ]
 },
 {path:'', pathMatch: 'full', redirectTo: 'main'}
